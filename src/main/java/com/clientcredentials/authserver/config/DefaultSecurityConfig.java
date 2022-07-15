7ebdacead77231927e9454dcef6d3c4f4aead0fe
Amal <amal.naeem@aurorasolutions.io>
2022-07-15 16:45:32 +0500

    Authorization Server with client credentials grant

---
+++ src/main/java/com/clientcredentials/authserver/config/DefaultSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.clientcredentials.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+          authorizeRequests.anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+}
